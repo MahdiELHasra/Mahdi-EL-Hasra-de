4f5602646e58d9676cd9e42ca985c1ea6e81bbb6
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-03-17 17:05:01 +0000

    feat: Add Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,87 @@
+import "../index.css";
+import Waving from "../../public/waving.png";
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+
+const skillsIcons = [
+  {
+    img: "https://skillicons.dev/icons?i=html,css",
+    id: 1,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=js,ts",
+    id: 2,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=react,next",
+    id: 3,
+  },
+
+  {
+    img: "https://skillicons.dev/icons?i=mysql,mongo",
+    id: 5,
+  },
+  {
+    img: "https://skillicons.dev/icons?i=python,",
+    id: 4,
+  },
+];
+
+function Hero() {
+  return (
+    <>
+      <section id="home" className="hero">
+        <div className="container">
+          <div className="content">
+            <div className="hero-main">
+              <div className="hero-text">
+                <h1 className="font-bold">Angehender Entwickler</h1>
+                <img src={Waving} alt="waving_hand" />
+                <p>
+                  Hi, mein Name ist Mahdi EL Hasra. Ich bin ein
+                  leidenschaftlicher angehender Entwickler mit Sitz in Rabat,
+                  Marokko. 📍
+                </p>
+                <span>
+                  <a
+                    aria-label="linkedin"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/mahdi-elhasra/"
+                  >
+                    <IconBrandLinkedin width={32} height={32} />
+                  </a>
+                  <a
+                    aria-label="github"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/MahdiELHasra"
+                  >
+                    <IconBrandGithub width={32} height={32} />
+                  </a>
+                </span>
+              </div>
+
+              <div className="hero-img"></div>
+            </div>
+
+            {/*  */}
+            <div className="skills">
+              <p>Tech Stack</p>
+              <div className="logos">
+                <ul>
+                  {skillsIcons.map((icon) => (
+                    <li key={icon.id}>
+                      <img src={icon.img} alt="skill-icon" />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Hero;
