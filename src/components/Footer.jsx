b242ffe025f519226ea91973c460e9532c3cbc55
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-03-17 17:04:29 +0000

    feat: Add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons-react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <div className="footerc">
+            <h3 className="font-bold">
+              Copyright © {new Date().getFullYear()}. Alle Rechte sind
+              vorbehalten.
+            </h3>
+            <div className="footerc__socials">
+              <a
+                aria-label="linkedin"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/mahdi-elhasra/"
+              >
+                <IconBrandLinkedin width={30} height={30} />
+              </a>
+              <a
+                aria-label="github"
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/MahdiELHasra"
+              >
+                <IconBrandGithub width={30} height={30} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
