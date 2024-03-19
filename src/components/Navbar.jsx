f8a9df94b0f53bcc5e33ffcf71c87817247c6862
El hasra mahdi <114767917+MehdiEL7asra@users.noreply.github.com>
2024-03-19 16:39:08 +0000

    Add Logo  to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "../index.css";
 import { IconMenu2, IconX } from "@tabler/icons-react";
-
+import Logo from "./logo.svg";
 function Navbar() {
   const [hamburger, setHamburger] = useState(false);
 
@@ -36,7 +36,7 @@ function Navbar() {
     <>
       <nav>
         <h3 onClick={pageUp} className="logo">
-          <img src="/logo.svg" alt="Mahdi EL Hasra logo" />
+          <img src={Logo} alt="Mahdi EL Hasra logo" />
         </h3>
         <ul>
           {navlinks.map((item) => (
